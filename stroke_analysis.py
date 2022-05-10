1cf07a915dac3d0dd9b43455595d88b892b4289c
fwanguem <fernando.wanguemert-guerra@altran.com>
2022-05-10 17:30:01 +0200

    Completed cov and PCA analysis, pending to act accordingly to the conclusions of it

--- stroke_analysis.py
+++ stroke_analysis.py
@@ -1,8 +1,9 @@
-import pandas # Imports the csv data
+import pandas  # Imports the csv data
 
 from pandas import DataFrame
 import matplotlib.pyplot as plt
 import numpy as np
+import seaborn
 
 
 df: DataFrame = pandas.read_csv("healthcare-dataset-stroke-data.csv")
@@ -10,6 +11,8 @@ df_array = df.to_numpy()
 
 # Discarding not numerical values in bmi
 discarding = pandas.isna(df["bmi"])
+# Extracting the names of the head in the dataframe:
+
 
 print(100 * '-')
 # Análisis lógico
@@ -20,6 +23,7 @@ NNon_stroked_patients = sum(df_array[:, -1] == 0)
 Males                 = (df_array[:, 1] == "Male") | (df_array[:, 1] == "Other")
 Females               = df_array[:, 1] == "Female"
 bmi_incomplete        = discarding.to_numpy()
+
 print(sum(bmi_incomplete))
 P_stroked_pacients = NStroked_patients / (NNon_stroked_patients + NStroked_patients)
 
@@ -59,21 +63,62 @@ Young_patients = df_array[:, 2] < 55
 Old_patients_glucose = df_array[Old_patients, 8]
 Young_patients_glucose = df_array[Young_patients, 8]
 
-#Reformating data for a cuantitative analysis
+# Reformating data for a cuantitative analysis
 
-df_array[Males,1] = 0
-df_array[Females,1] = 1
+df_array[Males, 1] = 0
+df_array[Females, 1] = 1
 
-df_array_PCA = df_array[~bmi_incomplete,:]
-df_array_PCA = df_array_PCA[:,[1,2,3,4,8,9]]
+df_array_cov_matrix = df_array[~bmi_incomplete, :]
+selected_atributes = [2, 3, 4, 8, 9,-1]
+selected_atributes_name = ['Gender','Age','hypertension','heart disease','glucose level','Stroke']
+df_array_cov_matrix = df_array_cov_matrix[:, selected_atributes]
 
-feat_means = np.mean(df_array_PCA,axis = 0)
 
-centered_df_array_PCA = df_array_PCA - feat_means
 
-correlation_matrix = np.corrcoef(centered_df_array_PCA.astype(float).T)
+
+feat_means = np.mean(df_array_cov_matrix, axis=0)
+centered_df_array = df_array_cov_matrix - feat_means
+std_df_array          = np.std(centered_df_array.astype(float), axis=0)
+
+#We now obtained the NORMALIZED dataset
+
+normalized_df_array = centered_df_array / std_df_array
+
+# The PCA analysis is to be performed on the feature space, not taking into account output information (Stroke)
+normalized_df_array_PCA = normalized_df_array[:,:-1]
+
+
+correlation_matrix = np.cov(normalized_df_array.astype(float).T)
+correlation_matrix_PCA = np.cov(normalized_df_array_PCA.astype(float).T)
+values, vectors = np.linalg.eig(correlation_matrix_PCA)
+
+eig_order = np.argsort(-values)
+
+sorted_values = values[eig_order]
+total_variance = sum(sorted_values)
+sorted_vectors = vectors[:,eig_order]
+
+
+
+
 
 print("***********************************************") # For debuging purposes
+
+#Results representation 
+
+# EigenValues of the feature variables selected for the analysis.
+plt.scatter(np.arange(len(sorted_values)),sorted_values)
+plt.title("Sorted Eigenvalues of the Covariance matrix")
+plt.ylabel("% of total variance")
+plt.show()
+
+ax1 = seaborn.heatmap(correlation_matrix,annot = True, linewidth = 0.5)
+plt.title('Correlation Matrix',fontsize = 15)
+ax1.set_xticks(np.arange(6) + 0.5)
+ax1.set_xticklabels(selected_atributes_name, rotation='vertical', fontsize=10)
+ax1.set_yticks(np.arange(6) + 0.5)
+ax1.set_yticklabels(selected_atributes_name, rotation='horizontal', fontsize=10)
+plt.show()
 
 fig, axs = plt.subplots(2, 2)
 
