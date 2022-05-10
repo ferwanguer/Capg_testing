7d108c09ac08fff853211963d49a45848b4c443e
fwanguem <fernando.wanguemert-guerra@altran.com>
2022-05-10 12:37:17 +0200

    Computation of cov matrix for PCA analysis

--- stroke_analysis.py
+++ stroke_analysis.py
@@ -2,19 +2,25 @@ import pandas # Imports the csv data
 
 from pandas import DataFrame
 import matplotlib.pyplot as plt
-
+import numpy as np
 
 
 df: DataFrame = pandas.read_csv("healthcare-dataset-stroke-data.csv")
 df_array = df.to_numpy()
 
+# Discarding not numerical values in bmi
+discarding = pandas.isna(df["bmi"])
+
 print(100 * '-')
 # Análisis lógico
-Stroked_patients = df_array[:, -1] == 1
-Diseased_patients = df_array[:, 4] == 1
-NStroked_patients = sum(Stroked_patients)
+Stroked_patients      = df_array[:, -1] == 1
+Diseased_patients     = df_array[:, 4] == 1
+NStroked_patients     = sum(Stroked_patients)
 NNon_stroked_patients = sum(df_array[:, -1] == 0)
-
+Males                 = (df_array[:, 1] == "Male") | (df_array[:, 1] == "Other")
+Females               = df_array[:, 1] == "Female"
+bmi_incomplete        = discarding.to_numpy()
+print(sum(bmi_incomplete))
 P_stroked_pacients = NStroked_patients / (NNon_stroked_patients + NStroked_patients)
 
 Smokers = (df_array[:, 10] == 'smokes') | (df_array[:, 10] == 'formerly smoked')
@@ -53,6 +59,22 @@ Young_patients = df_array[:, 2] < 55
 Old_patients_glucose = df_array[Old_patients, 8]
 Young_patients_glucose = df_array[Young_patients, 8]
 
+#Reformating data for a cuantitative analysis
+
+df_array[Males,1] = 0
+df_array[Females,1] = 1
+
+df_array_PCA = df_array[~bmi_incomplete,:]
+df_array_PCA = df_array_PCA[:,[1,2,3,4,8,9]]
+
+feat_means = np.mean(df_array_PCA,axis = 0)
+
+centered_df_array_PCA = df_array_PCA - feat_means
+
+correlation_matrix = np.corrcoef(centered_df_array_PCA.astype(float).T)
+
+print("***********************************************") # For debuging purposes
+
 fig, axs = plt.subplots(2, 2)
 
 # First figure
@@ -82,3 +104,18 @@ axs[1, 1].legend(loc='upper right')
 
 plt.show()
 print(100 * '*')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
